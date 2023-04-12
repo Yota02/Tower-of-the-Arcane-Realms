941128735fd2c12237ebc2c70e79806b2b215397
Yota002 <amichauxkinet@gmail.com>
2023-04-12 14:50:58 +0200

    class personnage

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import pygame
 
 pygame.init()
-
 pygame.display.set_caption("Tower of the Arcane Realms")
-pygame.display.set_mode((500, 300))
 
-running = True 
+ecran_taille = pygame.display.Info().current_w, pygame.display.Info().current_h
+screen = pygame.display.set_mode(ecran_taille)
+background = pygame.image.load('assets/background/48.jpg')
 
-while running: 
+
+running = True
+
+while running:
+
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
             print("fermeture du jeu")
+    
+    pygame.display.update()
+
+
+
+
